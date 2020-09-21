708fc27c5b6f719dfd6443779456e512e432b8c2
Jeff-Adler <Jeffm.adler@gmail.com>
2020-09-21 15:34:40 -0400

    fixed list view

--- src/screens/PostList.js
+++ src/screens/PostList.js
@@ -49,8 +49,8 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    // justifyContent: "center",
+    alignItems: "stretch",
   },
 });
 
