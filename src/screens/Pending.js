0fc11fca3620c89288d20c003b3f6306b7ecbb86
Jeff-Adler <Jeffm.adler@gmail.com>
2020-09-23 17:43:26 -0400

    added card for eligibles

--- src/screens/Pending.js
+++ src/screens/Pending.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
-import { Card, Text, Button } from "react-native-elements";
+import { Card, Text, Button, Icon } from "react-native-elements";
 import UserAvatar from "react-native-user-avatar";
 
 class Pending extends React.Component {
@@ -15,7 +15,42 @@ class Pending extends React.Component {
       <View style={styles.container}>
         {pendingUser !== null ? (
           <View style={styles.container}>
-            <Text h4>
+            <Card style={styles.miniContainer}>
+              <Card.Title>{`${pendingUser.first_name} ${pendingUser.last_name}`}</Card.Title>
+              <Card.Divider />
+              {/* <Card.Image source={require("../images/pic2.jpg")} /> */}
+              <Text style={styles.cardText}>
+                {pendingUser.username}
+                {"\n"}
+              </Text>
+              <Text style={styles.cardText}>
+                {pendingUser.gender}
+                {"\n"}
+              </Text>
+              <Text style={styles.cardText}>
+                {pendingUser.birthdate}
+                {"\n"}
+              </Text>
+              <UserAvatar
+                style={styles.avatar}
+                size={50}
+                bgColor="#3498db"
+                name={`${pendingUser.first_name} ${pendingUser.last_name}`}
+              />
+              <Text>{"\n"}</Text>
+              <Button
+                onPress={this.pressHandler}
+                // icon={<Icon name="code" color="#ffffff" />}
+                buttonStyle={{
+                  borderRadius: 0,
+                  marginLeft: 0,
+                  marginRight: 0,
+                  marginBottom: 0,
+                }}
+                title="Accept Mentee"
+              />
+            </Card>
+            {/* <Text h4>
               {pendingUser.username}
               {"\n"}
             </Text>
@@ -38,7 +73,7 @@ class Pending extends React.Component {
               name={`${pendingUser.first_name} ${pendingUser.last_name}`}
             />
             <Text>{"\n"}</Text>
-            <Button title="Accept Mentee" onPress={this.pressHandler} />
+            <Button title="Accept Mentee" onPress={this.pressHandler} /> */}
           </View>
         ) : null}
       </View>
@@ -51,14 +86,25 @@ const styles = StyleSheet.create({
     // flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    textAlign: "center",
+    // textAlign: "center",
     width: 50,
     height: 50,
     // marginBottom: 250,
     // paddingHorizontal: 24,
+    marginLeft: "35%",
+  },
+  cardText: {
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
   },
   container: {
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  miniContainer: {
+    // flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
