2a16cac94ee86e21668e2543653d8d698cf7fff6
Jeff-Adler <Jeffm.adler@gmail.com>
2020-09-27 11:06:16 -0400

    put card in profile show page

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
-import { Button, Text } from "react-native-elements";
+import { Button, Text, Card } from "react-native-elements";
 import UserAvatar from "react-native-user-avatar";
 var moment = require("moment");
 
 class Profile extends React.Component {
+  capitalize = (gender) => {
+    if (typeof gender !== "string") return "";
+    return gender.charAt(0).toUpperCase() + gender.slice(1);
+  };
   convertBirthdateToDate = (birthdate) => {
     const convertedBirthdate = moment(birthdate, "YYYY-MM-DD").format(
       "MMMM Do, YYYY"
     );
     return convertedBirthdate;
+  };
+
+  convertBirthdateToAge = (birthdate) => {
+    return moment().diff(birthdate, "years");
   };
 
   render() {
@@ -27,7 +35,57 @@ class Profile extends React.Component {
           </View>
         ) : (
           <View style={styles.miniContainer}>
-            <Text style={{ fontWeight: "bold" }} h4>
+            <Card style={styles.miniContainer}>
+              <Card.Title
+                style={styles.cardTitle}
+              >{`${currentUser.first_name} ${currentUser.last_name}`}</Card.Title>
+              <Card.Divider />
+              {/* <Card.Image source={require("../images/pic2.jpg")} /> */}
+              <UserAvatar
+                style={styles.avatar}
+                size={200}
+                bgColor="#3498db"
+                name={`${currentUser.first_name} ${currentUser.last_name}`}
+                src={currentUser.avatar}
+              />
+              <Text style={styles.italicizedText}>
+                {currentUser.description}
+                {"\n"}
+              </Text>
+              <Text style={styles.cardText}>
+                <Text style={{ fontWeight: "bold" }}>Gender: </Text>
+                {this.capitalize(currentUser.gender)}
+                {"         "}
+                <Text style={{ fontWeight: "bold" }}>Age: </Text>
+                {this.convertBirthdateToAge(currentUser.birthdate)}
+                {"\n"}
+              </Text>
+              {/* <Text style={styles.cardText}>
+                <Text style={{ fontWeight: "bold" }}>Username: </Text>
+                {pendingUser.username}
+                {"\n"}
+              </Text> */}
+              {/* <Text style={styles.cardText}>
+                <Text style={{ fontWeight: "bold" }}>Gender: </Text>
+                {this.capitalize(pendingUser.gender)}
+                {"\n"}
+              </Text> */}
+              {/* <Text style={styles.cardText}>
+                {this.convertBirthdateToDate(pendingUser.birthdate)}
+                {"\n"}
+              </Text> */}
+              {/* <Text style={styles.cardText}>
+                <Text style={{ fontWeight: "bold" }}>Age: </Text>
+                {this.convertBirthdateToAge(pendingUser.birthdate)}
+                {"\n"}
+              </Text> */}
+              <Button
+                style={styles.button}
+                title="Edit Profile"
+                onPress={() => this.props.navigation.navigate("Name")}
+              />
+            </Card>
+            {/* <Text style={{ fontWeight: "bold" }} h4>
               {`${currentUser.first_name} ${currentUser.last_name}`}
               {"\n"}
             </Text>
@@ -51,16 +109,11 @@ class Profile extends React.Component {
               name={`${currentUser.first_name} ${currentUser.last_name}`}
               src={"https://randomuser.me/api/portraits/men/44.jpg"}
             />
-            <Text>{"\n"}</Text>
+            <Text>{"\n"}</Text> */}
           </View>
         )}
         <Button
-          style={styles.button}
-          title="Edit Profile"
-          onPress={() => this.props.navigation.navigate("Name")}
-        />
-        <Button
-          style={styles.button}
+          style={styles.logoutButton}
           title="Logout"
           type="clear"
           onPress={() => logoutHandler()}
@@ -71,15 +124,47 @@ class Profile extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  avatar: {
+    // flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    width: 200,
+    height: 200,
+    marginTop: "5%",
+    marginBottom: "7%",
+    marginLeft: "12%",
   },
   button: {
     alignItems: "center",
   },
+  cardText: {
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+    fontSize: 20,
+  },
+  cardTitle: {
+    fontSize: 20,
+  },
+  container: {
+    // flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: "2%",
+  },
+  italicizedText: {
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+    fontStyle: "italic",
+    fontSize: 20,
+  },
+  logoutButton: {
+    marginTop: 12,
+    alignItems: "center",
+  },
   miniContainer: {
+    // flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
